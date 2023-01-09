48a0fb7eab9b93bc8236eff85d93fae0157ac87e
Rahdeg <walett95@gmail.com>
2023-01-09 18:56:38 +0100

    fixed-styles

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -61,11 +61,11 @@ const login = async (e)=>{
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
                   <div>
-                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email <span className='m-2'> is Cristian_Schmeler70@yahoo.com</span></label>
                       <input type="email" name="email" id="email" value={email} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" onChange={(e)=>setEmail(e.target.value)} />
                   </div>
                   <div>
-                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password <span className='m-2'>is Password</span></label>
                       <div className='flex flex-row items-center justify-between bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
                       <input type="password" name="password" id="password" placeholder="password" className="  sm:text-sm  focus:ring-primary-600 block w-full outline-none dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 bg-gray-50" value={password} onChange={(e)=>setPassword(e.target.value)}/>
                       <span className='rounded-lg text-lighttext cursor-pointer' onClick={()=>showPass()}>SHOW</span>
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import {BsArrowLeft} from 'react-icons/bs'
 import {AiOutlineStar,AiFillStar} from 'react-icons/ai'
-import IMG from '../Images/image 4 (1).png'
 import { useStateValue } from '../context/contexts/contextProvider';
 import { Link } from 'react-router-dom';
 import { getUser } from '../Api/Apicalls';
@@ -31,7 +30,6 @@ const Profile = () => {
      {
       userinfo && (
         <>
-        {console.log('user',userinfo)}
         <Link
         to={"/dashboard/users"}
         >
@@ -40,7 +38,7 @@ const Profile = () => {
       </div>
       </Link>
       <div className='flex items-center justify-between mt-3'>
-      <p>User Details</p>
+      <p className=' text-[#213F7D] text-base font-bold'>User Details</p>
     <div className='flex items-center justify-center'>
     <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
   <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
@@ -57,27 +55,27 @@ const Profile = () => {
     <div className='flex flex-col  gap-8 bg-white rounded-md px-3 mx-3 pt-4 mt-4'>
       <div className='flex items-center '>
       <div className='flex items-center justify-start gap-2 border-solid border-r-2  border-cyan-50'>
-      <img className="w-10 h-10 rounded-full" src={IMG} alt="" id='profile__img'/>
+      <img className="w-10 h-10 rounded-full" src={userinfo.profile.avatar} alt="" id='profile__img'/>
       <div className='flex flex-col items-start justify-center'>
-        <p>{userinfo.userName} </p>
+        <p className='text-[#213F7D] text-base font-bold'>{userinfo.userName} </p>
         <p>HG739899</p>
       </div>
       <div className=''> </div>
       </div>
       <div className='flex  items-center justify-center gap-3 ml-3 border-solid border-r-2  border-cyan-50'>
         <div className='flex flex-col items-start justify-center gap-3'>
-        <p>User's Tier</p>
+        <p className='text-[#213F7D] text-base font-bold'>User's Tier</p>
         <p className='flex items-center justify-center'><AiFillStar/> <AiOutlineStar/> <AiOutlineStar/></p>
         </div>
         <div className=''> </div>
       </div>
       <div className='flex flex-col items-start ml-3 justify-start'>
-      <p>{`# ${userinfo.accountBalance}`}</p>
+      <p className='text-[#213F7D] text-base font-bold'>{`#${userinfo.accountBalance}`}</p>
       <p>{userinfo.accountNumber} /<span>Providus Bank</span></p>
       </div>
       </div>
-    <div className='flex items-center justify-around'>
-    <p className='border-solid border-b-2  border-cyan-50'>General Details</p>
+    <div className='flex items-center justify-around mt-3'>
+    <p className='border-solid border-b-2  border-[#39CDCC] text-[#39CDCC]'>General Details</p>
     <p className=''>Documents</p>
     <p className=''>Bank Details</p>
     <p className=''>Loans</p>
@@ -86,63 +84,63 @@ const Profile = () => {
     </div>
     </div>
     <div className='flex flex-col  gap-8 bg-white rounded-md px-3 mx-3 pt-4 mt-4'>
-    <div className='flex flex-col items-start gap-5  border-solid border-b-2  border-cyan-50'>
-    <p>Personal Information</p>
-    <div className='flex items gap-28'>
-      <div className='flex flex-col items-start justify-center'><p className=''>FULL NAME</p> <p>{userinfo.profile.firstName}<span className=' ml-2'>{userinfo.profile.lastName}</span> </p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>PHONE NUMBER</p> <p>{userinfo.profile.phoneNumber} </p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''> EMAIL ADDRESS</p> <p>{userinfo.email}</p></div>
-      <div className='flex flex-col items-start justify-center'> <p className=''>BVN</p> <p>{userinfo.profile.bvn}</p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>GENDER</p> <p>{userinfo.profile.gender}</p></div>
+    <div className='flex flex-col items-start gap-5  border-solid border-b-2  border-[#39CDCC]'>
+    <p className='text-[#213F7D] text-base font-bold'>Personal Information</p>
+    <div className='flex items gap-20 mb-2'>
+      <div className='flex flex-col items-start justify-center'><p className=''>FULL NAME</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.profile.firstName}<span className=' ml-2'>{userinfo.profile.lastName}</span> </p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>PHONE NUMBER</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.profile.phoneNumber} </p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''> EMAIL ADDRESS</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.email}</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>BVN</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.profile.bvn}</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>GENDER</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.profile.gender}</p></div>
     </div>
-    <div className='flex items-start gap-32'>
-    <div className='flex flex-col items-start justify-center'><p className=''>MARITAL STATUS</p> <p>SINGLE </p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>CHILDREN</p> <p> None</p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''> TYPE OF RESIDENT</p> <p>Parent's Apartment</p></div>
+    <div className='flex items-start gap-32 mb-2'>
+    <div className='flex flex-col items-start justify-center'><p className=''>MARITAL STATUS</p> <p className='text-[#213F7D] text-base font-bold'>SINGLE </p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>CHILDREN</p> <p className='text-[#213F7D] text-base font-bold'> None</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''> TYPE OF RESIDENT</p> <p className='text-[#213F7D] text-base font-bold'>Parent's Apartment</p></div>
     </div>
     <div className=''></div>
     </div>
-    <div className='flex flex-col items-start gap-5  border-solid border-b-2  border-cyan-50'>
-    <p>Education and Employement</p>
+    <div className='flex flex-col items-start gap-5  border-solid border-b-2  border-[#39CDCC]'>
+    <p className='text-[#213F7D] text-base font-bold'>Education and Employement</p>
     <div className='flex items gap-24'>
-      <div className='flex flex-col items-start justify-center'><p className=''>LEVEL OF EDUCATION</p> <p>{userinfo.education.level}</p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>EMPLOYEMENT STATUS</p> <p>{userinfo.education.employmentStatus}</p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''> SECTOR OF EMPLOYEMENT</p> <p>{userinfo.education.sector}</p></div>
-      <div className='flex flex-col items-start justify-center'> <p className=''>DURATION OF EMPLOYEMENT</p> <p>{userinfo.education.duration}</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>LEVEL OF EDUCATION</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.education.level}</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>EMPLOYEMENT STATUS</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.education.employmentStatus}</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''> SECTOR OF EMPLOYEMENT</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.education.sector}</p></div>
+      <div className='flex flex-col items-start justify-center'> <p className=''>DURATION OF EMPLOYEMENT</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.education.duration}</p></div>
     </div>
-    <div className='flex items-start gap-32'>
-    <div className='flex flex-col items-start justify-center'><p className=''>OFFICE EMAIL</p> <p>{userinfo.education.officeEmail} </p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>MONTHLY INCOME</p> <p> {`#${userinfo.education.monthlyIncome[0]}`}<span>-</span><span>{`#${userinfo.education.monthlyIncome[1]}`}</span></p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''> LOAN REPAYMENT</p> <p> {userinfo.education.loanRepayment}</p></div>
-    </div>
-    <div className=''></div>
-    </div>
-    <div className='flex flex-col items-start gap-5  border-solid border-b-2  border-cyan-50'>
-    <p>Socials</p>
-    <div className='flex items-start gap-32'>
-    <div className='flex flex-col items-start justify-center'><p className=''>TWITTER</p> <p>{userinfo.socials.twitter} </p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>FACEBOOK</p> <p>{userinfo.socials.facebook}</p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>INSTAGRAM</p> <p> {userinfo.socials.instagram}</p></div>
+    <div className='flex items-start gap-32 mb-2'>
+    <div className='flex flex-col items-start justify-center'><p className=''>OFFICE EMAIL</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.education.officeEmail} </p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>MONTHLY INCOME</p> <p className='text-[#213F7D] text-base font-bold'> {`#${userinfo.education.monthlyIncome[0]}`}<span>-</span><span className='text-[#213F7D] text-base font-bold'>{`#${userinfo.education.monthlyIncome[1]}`}</span></p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''> LOAN REPAYMENT</p> <p className='text-[#213F7D] text-base font-bold'> {userinfo.education.loanRepayment}</p></div>
     </div>
     <div className=''></div>
     </div>
-    <div className='flex flex-col items-start gap-5  border-solid border-b-2  border-cyan-50'>
-    <p>Guarantor</p>
-    <div className='flex items-start gap-32'>
-    <div className='flex flex-col items-start justify-center'><p className=''>FULL NAME</p> <p>{userinfo.guarantor.firstName}<span className=' ml-2'>{userinfo.guarantor.lastName}</span></p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>PHONE NUMBER</p> <p>{userinfo.guarantor.phoneNumber}</p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>EMAIL ADDRESS</p> <p> {userinfo.guarantor.address}</p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>RELATIONSHIP</p> <p> Sister</p></div>
+    <div className='flex flex-col items-start gap-5  border-solid border-b-2  border-[#39CDCC]'>
+    <p className='text-[#213F7D] text-base font-bold'>Socials</p>
+    <div className='flex items-start gap-32 mb-2'>
+    <div className='flex flex-col items-start justify-center'><p className=''>TWITTER</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.socials.twitter} </p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>FACEBOOK</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.socials.facebook}</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>INSTAGRAM</p> <p className='text-[#213F7D] text-base font-bold'> {userinfo.socials.instagram}</p></div>
     </div>
     <div className=''></div>
     </div>
-    <div className='flex flex-col items-start gap-5  border-solid border-b-2  border-cyan-50'>
+    <div className='flex flex-col items-start gap-5  border-solid border-b-2  border-[#39CDCC]'>
+    <p className='text-[#213F7D] text-base font-bold'>Guarantor</p>
+    <div className='flex items-start gap-32 mb-2'>
+    <div className='flex flex-col items-start justify-center'><p className=''>FULL NAME</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.guarantor.firstName}<span className=' ml-2 text-[#213F7D] text-base font-bold'>{userinfo.guarantor.lastName}</span></p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>PHONE NUMBER</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.guarantor.phoneNumber}</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>EMAIL ADDRESS</p> <p className='text-[#213F7D] text-base font-bold'> {userinfo.guarantor.address}</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>RELATIONSHIP</p> <p className='text-[#213F7D] text-base font-bold'> Sister</p></div>
+    </div>
+    <div className=''></div>
+    </div>
+    <div className='flex flex-col items-start gap-5  '>
     <p></p>
-    <div className='flex items-start gap-32'>
-    <div className='flex flex-col items-start justify-center'><p className=''>FULL NAME</p> <p>{userinfo.guarantor.firstName}<span className=' ml-2'>{userinfo.guarantor.lastName}</span> </p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>PHONE NUMBER</p> <p>{userinfo.guarantor.phoneNumber}</p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>EMAIL ADDRESS</p> <p>{userinfo.guarantor.address}</p></div>
-      <div className='flex flex-col items-start justify-center'><p className=''>RELATIONSHIP</p> <p> Sister</p></div>
+    <div className='flex items-start gap-32 mb-3'>
+    <div className='flex flex-col items-start justify-center'><p className=''>FULL NAME</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.guarantor.firstName}<span className=' ml-2'>{userinfo.guarantor.lastName}</span> </p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>PHONE NUMBER</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.guarantor.phoneNumber}</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>EMAIL ADDRESS</p> <p className='text-[#213F7D] text-base font-bold'>{userinfo.guarantor.address}</p></div>
+      <div className='flex flex-col items-start justify-center'><p className=''>RELATIONSHIP</p> <p className='text-[#213F7D] text-base font-bold'> Sister</p></div>
     </div>
     <div className=''></div>
     </div>
